7856b5d63306e95f2729d9e7cc84b984acbf6723
Myles <mylearagay@gmail.com>
2022-10-12 12:23:41 +0100

    Solved Best Time to Buy and Sell Stocks
    2/75, refer back to LeetCode 121

---
+++ solutions/best time to buy and sell stocks.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l = 0  # Points to min
+        r = 1  # Points to max
+        maxProfit = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                if profit > maxProfit:
+                    maxProfit = profit
+            else:
+                l = r
+            r += 1
+
+        return maxProfit
